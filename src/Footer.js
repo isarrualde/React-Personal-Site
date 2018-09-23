ab68f67d9c90ff224619b808450b842540b9ff75
Matias I <isarrualde@gmail.com>
2018-09-23 10:37:36 -0300

    plus Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+ 
+class Footer extends Component {
+  render() {
+    return (
+      //<!-- Footer -->
+    <footer>
+    <div className="container">
+      <div className="units-row">
+          <div className="unit-50">
+            <p>Matias Isarrualde 2018</p>
+          </div>
+          <div className="unit-50">
+          <ul className="social list-flat right">
+            <li><a target="_blank"  href="mailto:isarrualde@gmail.com"><i className="fas fa-at"></i></a></li>
+            <li><a target="_blank"  href="http://twitter.com/isarrualde"><i className="fab fa-twitter"></i></a></li>
+            <li><a target="_blank"  href="http://behance.com/isarrualde"><i className="fab fa-behance"></i></a></li>
+            <li><a target="_blank"  href="https://github.com/isarrualde"><i className="fab fa-github"></i></a></li>
+          </ul>
+          </div>
+      </div>
+    </div>
+  </footer>
+
+    );
+  }
+}
+ 
+export default Footer;
+
